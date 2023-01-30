5a96cb649d6d29a8299b23f4c358c08634f00275
rishirahuln <nrishirahul@gmail.com>
2023-01-30 21:17:49 +0530

    modified cookie with sameSite none

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const { isAuth } = require("./middlewares/authentication");
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Welcome!!!");
