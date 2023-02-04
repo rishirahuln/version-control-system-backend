26fdd03c83442f9a673804f93b3bb314d027d764
rishirahuln <nrishirahul@gmail.com>
2023-02-04 18:51:57 +0530

    expiring cookie before clearing

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -60,8 +60,8 @@ const signin = async (req, res) => {
 
 const signout = async (req, res) => {
   try {
-    await res.clearCookie("accessToken");
     await res.cookie("accessToken", { expire: new Date() });
+    await res.clearCookie("accessToken");
     res.status(200).send({ message: "User signed-out successfully" });
   } catch (error) {
     res.status(500).send({ message: "Internal Server Error", error: error });
